c8cedfb760c8245119d8ff38cb789090fb54bbba
George Beckstein <george.beckstein@gmail.com>
2021-04-27 11:55:50 -0400

    Deprecate pretty printer, update to use mbed-trace
    This commit deprecates the functions in pretty printer and replaces them with new versions in ble_logging that use the standard mbed-trace library for BLE utility logging.
    
    A library should not use printf when a trace library is available that allows you to configure/enable/disable output. This prevents the debug output from being cluttered with unformatted debug messages from the BLE utility library.

--- pretty_printer.h
+++ pretty_printer.h
@@ -20,86 +20,28 @@
 #include <mbed.h>
 #include "ble/BLE.h"
 
+#include "platform/mbed_toolchain.h"
+
+#warning "pretty_printer.h has been replaced with ble_logging.h. Please include that file instead."
+#include "ble_logging.h"
+
+MBED_DEPRECATED("print_error is deprecated. Use ble_log_error instead.")
 inline void print_error(ble_error_t error, const char* msg)
 {
-    printf("%s: ", msg);
-    switch(error) {
-        case BLE_ERROR_NONE:
-            printf("BLE_ERROR_NONE: No error");
-            break;
-        case BLE_ERROR_BUFFER_OVERFLOW:
-            printf("BLE_ERROR_BUFFER_OVERFLOW: The requested action would cause a buffer overflow and has been aborted");
-            break;
-        case BLE_ERROR_NOT_IMPLEMENTED:
-            printf("BLE_ERROR_NOT_IMPLEMENTED: Requested a feature that isn't yet implement or isn't supported by the target HW");
-            break;
-        case BLE_ERROR_PARAM_OUT_OF_RANGE:
-            printf("BLE_ERROR_PARAM_OUT_OF_RANGE: One of the supplied parameters is outside the valid range");
-            break;
-        case BLE_ERROR_INVALID_PARAM:
-            printf("BLE_ERROR_INVALID_PARAM: One of the supplied parameters is invalid");
-            break;
-        case BLE_STACK_BUSY:
-            printf("BLE_STACK_BUSY: The stack is busy");
-            break;
-        case BLE_ERROR_INVALID_STATE:
-            printf("BLE_ERROR_INVALID_STATE: Invalid state");
-            break;
-        case BLE_ERROR_NO_MEM:
-            printf("BLE_ERROR_NO_MEM: Out of Memory");
-            break;
-        case BLE_ERROR_OPERATION_NOT_PERMITTED:
-            printf("BLE_ERROR_OPERATION_NOT_PERMITTED");
-            break;
-        case BLE_ERROR_INITIALIZATION_INCOMPLETE:
-            printf("BLE_ERROR_INITIALIZATION_INCOMPLETE");
-            break;
-        case BLE_ERROR_ALREADY_INITIALIZED:
-            printf("BLE_ERROR_ALREADY_INITIALIZED");
-            break;
-        case BLE_ERROR_UNSPECIFIED:
-            printf("BLE_ERROR_UNSPECIFIED: Unknown error");
-            break;
-        case BLE_ERROR_INTERNAL_STACK_FAILURE:
-            printf("BLE_ERROR_INTERNAL_STACK_FAILURE: internal stack failure");
-            break;
-        case BLE_ERROR_NOT_FOUND:
-            printf("BLE_ERROR_NOT_FOUND");
-            break;
-        default:
-            printf("Unknown error");
-    }
-    printf("\r\n");
+    ble_log_error(error, msg);
 }
 
 /** print device address to the terminal */
+MBED_DEPRECATED("print_address is deprecated. Use ble_log_address instead")
 inline void print_address(const ble::address_t &addr)
 {
-    printf("%02x:%02x:%02x:%02x:%02x:%02x\r\n",
-           addr[5], addr[4], addr[3], addr[2], addr[1], addr[0]);
+    ble_log_address(addr);
 }
 
+MBED_DEPRECATED("print_mac_address is deprecated. Use ble_log_local_mac_address instead")
 inline void print_mac_address()
 {
-    /* Print out device MAC address to the console*/
-    ble::own_address_type_t addr_type;
-    ble::address_t address;
-    BLE::Instance().gap().getAddress(addr_type, address);
-    printf("DEVICE MAC ADDRESS: ");
-    print_address(address);
-}
-
-inline const char* phy_to_string(ble::phy_t phy) {
-    switch(phy.value()) {
-        case ble::phy_t::LE_1M:
-            return "LE 1M";
-        case ble::phy_t::LE_2M:
-            return "LE 2M";
-        case ble::phy_t::LE_CODED:
-            return "LE coded";
-        default:
-            return "invalid PHY";
-    }
+    ble_log_local_mac_address();
 }
 
 #endif /* PRETTY_PRINTER_H_ */
