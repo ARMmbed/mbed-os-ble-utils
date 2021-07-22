886a53d7555face83716bb893aba5394a5270d1a
Paul Szczepanek <pawel.szczepanek@arm.com>
2021-07-22 09:49:29 +0100

    missing argument of ble, remove deprecation
    No need to deprecate, this is not user API

--- pretty_printer.h
+++ pretty_printer.h
@@ -25,23 +25,20 @@
 #warning "pretty_printer.h has been replaced with ble_logging.h. Please include that file instead."
 #include "ble_logging.h"
 
-MBED_DEPRECATED("print_error is deprecated. Use ble_log_error instead.")
 inline void print_error(ble_error_t error, const char* msg)
 {
     ble_log_error(error, msg);
 }
 
 /** print device address to the terminal */
-MBED_DEPRECATED("print_address is deprecated. Use ble_log_address instead")
 inline void print_address(const ble::address_t &addr)
 {
     ble_log_address(addr);
 }
 
-MBED_DEPRECATED("print_mac_address is deprecated. Use ble_log_local_mac_address instead")
 inline void print_mac_address()
 {
-    ble_log_local_mac_address();
+    ble_log_local_mac_address(BLE::Instance());
 }
 
 #endif /* PRETTY_PRINTER_H_ */
